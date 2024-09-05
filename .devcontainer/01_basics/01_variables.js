5be4155e13064d1675a3a033a3410ae977dd9aaa
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-09-05 14:15:11 +0000

    class2 @ variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+//today we will learn about variables.
+const accountId = 131124; 
+let accountEmail = "nachiket@gmail.com";
+var accountPassword = "3231242dadsda";
+accountCity = "Nevada";
+
+//accountId = 2;   // not allowed
+console.log(accountId);   
+
+accountEmail = "dbajdad";
+accountPassword = "najndkanfa";
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);  //you can also console log multiple varaiables into using tables.
+
+/* if we want to store a constant variable in storage, only const will be used. Now, if we want a variable to storage, we can use let and var. let is much
+better as it supports scope in JS. you cannot use one variable from one scope into another. therefore, let is better and use that only. */
