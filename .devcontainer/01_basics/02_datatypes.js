b5fd033e1917da80a12937b5c5e9ce9ba02b8f8c
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-09-06 12:43:14 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+ "use strict"; //treat all js code as the newer version. if your code is old and now you want to use the new syntax in it.
+//  alert(3+3); // we are using node js not browser so it wont work.
+ 
+let name = "Nachiket" //string datatype
+let age = 21 //int datatype
+let isLoggedIn = false; //boolean datatype
+
+//null is a standalone value whereas undefined is a state in which that particular variable is not defined.
+//symbol => uniqueness 
+
+//object 
+console.log(typeof(name)); //tells the type of variable
+console.log(null);  //object
+console.log(undefined); //undefined
+
+
+
